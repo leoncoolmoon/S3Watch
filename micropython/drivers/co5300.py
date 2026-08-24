3a038524b51b003399a90049b490a7a687e27385
Jack <leoncoolmoon@hotmail.com>
2026-08-25 06:58:17 +1000

    Delete micropython/drivers directory

--- micropython/drivers/co5300.py
+++
@@ -1,9 +0,0 @@
-"""
-CO5300 AMOLED Display Driver wrapper for backwards compatibility
-"""
-
-from lib.hw.co5300 import CO5300 as _CO5300
-
-class CO5300(_CO5300):
-    def register_lvgl_display(self):
-        return self.create_lvgl_display()
